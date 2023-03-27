54c0f4b11ec9cf7550adc5c694eb704cab6678fc
hero <kdgzs2010@163.com>
2023-03-27 19:23:56 +0800

    debug

--- shangmen/models.py
+++ shangmen/models.py
@@ -1,3 +1,17 @@
+
+import os
 from django.db import models
 
-# Create your models here.
+
+def user_directory_path(instance, filename):
+    ext = filename.split('.').pop()
+    filename = '{0}{1}.{2}'.format(instance.name, instance.identity_card, ext)
+    return os.path.join(instance.major.name, filename) # 系统路径分隔符差异，增强代码重用性
+
+
+class HomeBar(models.Model):
+    id = models.AutoField(primary_key=True)
+    src= models.ImageField('照片', upload_to = user_directory_path, blank = True, null = True)
+
+    def __str__(self):
+        return str(self.sequence) + ':' + self.name
