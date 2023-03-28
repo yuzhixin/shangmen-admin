60b94b9c2c21dc2c215a3f45b449836e96b9321f
nero <yuzhixin@zhihu.com>
2023-03-28 08:12:27 +0800

    debug

--- shangmen/admin.py
+++ shangmen/admin.py
@@ -1,6 +1,44 @@
 from django.contrib import admin
-from models import HomeBar, ShopInfo
+from .models import HomeBar, ShopInfo, Shangpin
 
 
-admin.site.register(HomeBar)
-admin.site.register(ShopInfo)
+class HomeBarAdmin(admin.ModelAdmin):
+
+    # 列表页，列表顶部显示的字段名称
+    list_display = ('id', 'src')
+
+    # 列表页出现搜索框，参数是搜索的域
+    search_fields = ('id', 'src')
+
+    # 右侧会出现过滤器，根据字段类型，过滤器显示过滤选项
+    # list_filter = ('publication_date',)
+
+    # 页面中的列表顶端会有一个逐层深入的导航条，逐步迭代选项
+    # date_hierarchy = 'date'
+
+    # 自然是排序所用了，减号代表降序排列
+    ordering = ('-id',)
+
+
+class ShopInfoAdmin(admin.ModelAdmin):
+
+    # 列表页，列表顶部显示的字段名称
+    list_display = ('mobile', 'weixin')
+
+
+class ShangpinAdmin(admin.ModelAdmin):
+
+    # 列表页，列表顶部显示的字段名称
+    list_display = ('id', 'title', 'view_price',
+                    'real_price', 'tag', 'isPublish')
+
+    # 列表页出现搜索框，参数是搜索的域
+    search_fields = ('title', 'tag', 'isPublish')
+
+    # 自然是排序所用了，减号代表降序排列
+    ordering = ('-id',)
+
+
+admin.site.register(HomeBar, HomeBarAdmin)
+admin.site.register(ShopInfo, ShopInfoAdmin)
+admin.site.register(Shangpin, ShangpinAdmin)
--- shangmen/models.py
+++ shangmen/models.py
@@ -6,11 +6,11 @@ from ckeditor.fields import RichTextField
 
 class HomeBar(models.Model):
     id = models.AutoField(primary_key=True)
-    src = models.ImageField(verbose_name='首页轮播图', null=True,
-                            blank=True, upload_to='image', max_length=200)
+    src = models.ImageField(verbose_name='首页轮播图',
+                            upload_to='image', max_length=200)
 
-    def __str__(self):
-        return self.src
+    class Meta:
+        verbose_name_plural = '首页轮播图'
 
     def src_url(self):
         if self.src and hasattr(self.src, 'url'):
@@ -18,9 +18,41 @@ class HomeBar(models.Model):
         else:
             return HOSTS + '/media/default/user.jpg'
 
+
 class ShopInfo(models.Model):
     id = models.AutoField(primary_key=True)
-    mobile = models.CharField(max_length=128)
-    weixin = models.CharField(max_length=128)
-    address = models.CharField(max_length=256)
-    sumery = RichTextField()
+    mobile = models.CharField(max_length=128, verbose_name="手机号")
+    weixin = models.CharField(max_length=128, verbose_name="微信")
+    address = models.CharField(max_length=256, verbose_name="地址")
+    sumery = RichTextField(verbose_name="商店简介")
+
+    class Meta:
+        verbose_name_plural = '店铺信息'
+
+
+class Shangpin(models.Model):
+    TAG = [
+        ('ms', '秒杀'),
+        ('yh', '优惠'),
+        ('cx', '促销'),
+    ]
+
+    PUBLISH = [
+        (1, '发布'),
+        (0, '未发布'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=128, verbose_name="标题")
+    src = models.ImageField(verbose_name='头图',
+                            upload_to='image', max_length=200)
+    view_price = models.DecimalField(
+        verbose_name="对比价格", max_digits=5, decimal_places=2, blank=False, null=False)
+    real_price = models.DecimalField(
+        verbose_name="真实价格",  max_digits=5, decimal_places=2, blank=False, null=False)
+    tag = models.CharField(max_length=128, verbose_name="标签", choices=TAG)
+    sumery = RichTextField(verbose_name="商品介绍")
+    isPublish = models.IntegerField(choices=PUBLISH)
+
+    class Meta:
+        verbose_name_plural = '商品详情'
