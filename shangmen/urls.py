caa0d0f62397ff5d1ca84caf8e7e3418c3642f23
nero <yuzhixin@zhihu.com>
2023-03-30 08:26:47 +0800

    debug

---
+++ shangmen/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from shangmen import views
+
+urlpatterns = [
+    path('home_info/', views.home_info, name='home_info'),
+    path('shop_info/', views.shop_info, name='shop_info'),
+    path('shangpin_list/', views.shangpin_list, name='shangpin_list'),
+]
--- shangmen/views.py
+++ shangmen/views.py
@@ -2,9 +2,43 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from shangmen.models import HomeBar
+from shangmen.models import HomeBar, ShopInfo, Shangpin
+from django.http import JsonResponse
 
 
 def home_info(request):
     homeBar = HomeBar.objects.all()
-    
+    ret = []
+    for home in homeBar:
+        ret.append({
+            "src": home.src.name,
+            "content": "",
+        })
+    return JsonResponse({'code': 0, 'ret': ret, 'msg': ''})
+
+
+def shop_info(request):
+    shop = ShopInfo.objects.first()
+    ret = {
+        "mobile": shop.mobile,
+        "weixin": shop.weixin,
+        "address": shop.address,
+        "sumery": shop.sumery,
+    }
+    return JsonResponse({'code': 0, 'ret': ret, 'msg': ''})
+
+
+def shangpin_list(request):
+    shangpins = Shangpin.objects.all()
+    ret = []
+    for shangpin in shangpins:
+        ret.append({
+            "id": shangpin.id,
+            "src": shangpin.src.name,
+            "title": shangpin.title,
+            "view_price": shangpin.view_price,
+            "real_price": shangpin.real_price,
+            "tag": shangpin.tag,
+            "sumery": shangpin.sumery,
+        })
+    return JsonResponse({'code': 0, 'ret': ret, 'msg': ''})
