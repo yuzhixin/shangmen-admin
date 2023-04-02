3f1e2d11e73f04c2e6a52d3d1942d39b1833f79c
nero <yuzhixin@zhihu.com>
2023-04-02 09:33:15 +0800

    debug

--- shangmen_admin/settings.py
+++ shangmen_admin/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'shangmen_admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shangmen',
+        'USER': 'hero',
+        'PASSWORD': 'yU411416',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
