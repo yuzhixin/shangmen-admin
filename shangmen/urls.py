7beea5c3b31387c37b833bf2a3e531938de04e42
hero <kdgzs2010@163.com>
2023-08-11 17:55:22 +0800

    feat: add order api

--- shangmen/urls.py
+++ shangmen/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('add_address/', views.add_address, name='add_address'),
     path('update_address/', views.update_address, name='update_address'),
     path('create_order/', views.create_order, name='create_order'),
+    path('order_list/', views.order_list, name='order_list'),
 ]
