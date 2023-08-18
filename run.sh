2414605bb87c4c21c0a422116bac022aeb9e2405
hero <kdgzs2010@163.com>
2023-08-18 15:52:44 +0800

    debug

--- run.sh
+++ run.sh
@@ -1,3 +1,24 @@
-#!/bin/bash
+# #!/bin/bash
 
-gunicorn -w 1 -b 127.0.0.1:8888 shangmen:app --daemon 
+#进程名称
+process_name=shangmen_admin
+
+while [ 0 -eq 0 ]
+do
+    ps -ef|grep $process_name |grep -v grep
+    # $? -ne 0 不存在，$? -eq 0 存在
+    if [ $? -ne 0 ]
+    then
+        echo ">>>process is stop,to start"
+        #启动进程
+        nohup gunicorn --bind 0.0.0.0:8888 shangmen_admin.wsgi  &
+        
+        break
+    else
+        echo ">>>process is runing,to kill"
+        #停止进程
+        ps -ef | grep $process_name | grep -v grep | awk '{print $2}' | xargs kill
+        #休眠一秒后判断
+        sleep 1
+    fi
+done
